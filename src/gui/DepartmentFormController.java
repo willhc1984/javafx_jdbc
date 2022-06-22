ee212b328ee907832b9023bb8f4c10b4a7b889fe
will henrique <willhc1984@gmail.com>
2022-06-22 10:24:55 -0300

    Department - observer pattern - update table view

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -1,9 +1,12 @@
 package gui;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import db.DbException;
+import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Constraints;
 import gui.util.Utils;
@@ -22,6 +25,7 @@ public class DepartmentFormController implements Initializable{
 	private Department entity;
 	private DepartmentService service;
 	
+	private List<DataChangeListener> dataChangeListeners = new ArrayList<>();
 	
 	@FXML
 	public TextField txtId;
@@ -45,13 +49,19 @@ public class DepartmentFormController implements Initializable{
 		try {
 			entity = getFormData();
 			service.saveOrUpdate(entity);
+			notifyDataChangeListener();
 			Utils.currentStage(event).close();
 		} catch (DbException e) {
 			Alerts.showAlert("Error saving...", null, e.getMessage(), AlertType.ERROR);
-		}
-		
-		
+		}	
 	}
+	
+	private void notifyDataChangeListener() {
+		for(DataChangeListener listener : dataChangeListeners) {
+			listener.onDataChanged();
+		}
+	}
+
 	private Department getFormData() {
 		Department obj = new Department();
 		obj.setId(Utils.tryParseToInt(txtId.getText()));
@@ -63,7 +73,6 @@ public class DepartmentFormController implements Initializable{
 		Utils.currentStage(event).close();
 	}
 	
-
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 		inicializeNodes();
@@ -88,6 +97,10 @@ public class DepartmentFormController implements Initializable{
 		}
 		txtId.setText(String.valueOf(entity.getId()));
 		txtName.setText(entity.getName());
+	}
+	
+	public void subscribeDataChangeListener(DataChangeListener listener) {
+		dataChangeListeners.add(listener);
 	}
 
 }
